430639b6d95e41f87d771fa589616b1ea46d9428
Tejaswini <chennamsettitejaswini5@gmail.com>
2026-07-13 21:29:18 +0530

    prepare backend for cloud computing

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,26 @@
 require("dotenv").config();
 
-console.log("DB USER:", process.env.DB_USER);
-console.log("DB HOST:", process.env.DB_HOST);
 const express = require("express");
 const cors = require("cors");
 const db = require("./db");
 
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
+
+// Routes
 app.use("/api/feedback", require("./routes/feedback"));
 
+// Test route
 app.get("/", (req, res) => {
     res.send("Backend is running successfully!");
 });
 
-const PORT = 5000;
+// Cloud deployment port
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
